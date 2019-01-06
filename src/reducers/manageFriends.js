3db1b4037439cc3d314f768292d095299d5ceb59
Irum <irum@Irums-MacBook-Air.local>
2019-01-06 20:06:51 +0000

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,19 @@
-export function manageFriends(state, action){
+let state = { friends: [] };
+export function manageFriends(state, action) {
+  switch (action.type) {
+    case "ADD_FRIEND":
+      return { ...state, friends: [...state.friends, action.friend] };
+
+    // state = { ...state, friends: updatedFriends };
+    case "REMOVE_FRIEND":
+      debugger;
+
+      return {
+        ...state,
+        friends: state.friends.filter(friend => friend.id !== action.id)
+      };
+    // state = { ...state, friends: updatedFriends };
+    default:
+      return state;
+  }
 }
