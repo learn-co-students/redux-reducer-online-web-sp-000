d055772187988d1acd8aec658716f7e47f4cbecf
Kevin Peery <kevin.peery@gmail.com>
2019-03-22 21:31:53 -0500

    Done.

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -3,9 +3,19 @@ export function managePresents(state = {
 }, action) {
   switch (action.type) {
     case "INCREASE":
-      return Object.assign({}, state, {
-        numberOfPresents: state.numberOfPresents + 1
-      });
+      //refactored to use the spread operator instead of Object.assign
+      return (
+        {
+          ...state,
+          numberOfPresents: state.numberOfPresents + 1
+        }
+
+      )
+    //this is the older syntax using Object.assign():
+
+    // return Object.assign({}, state, {
+    //   numberOfPresents: state.numberOfPresents + 1
+    // });
 
     default:
       return state;
