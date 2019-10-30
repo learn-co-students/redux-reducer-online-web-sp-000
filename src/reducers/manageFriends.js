7e28bc7f1e5360285e07fad5e90e3bfc0d0bb892
Andrew Davisson <akdavisson4@gmail.com>
2019-10-30 09:04:25 -0600

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,12 @@
 export function manageFriends(state, action){
+
+  switch(action.type) {
+    case 'ADD_FRIEND':
+      state.friends.push(action.friend);
+      return state;
+    case 'REMOVE_FRIEND':
+      return {friends: state.friends.filter((value, index, arr) => {return value.id != action.id})}
+    default:
+      return state;
+  }
 }
