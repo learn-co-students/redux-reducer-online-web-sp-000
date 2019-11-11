552ded48c3a4128f9972dd39060cd8da54a50805
github username <github email address>
2019-11-11 13:23:57 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,27 @@
 export function manageFriends(state, action){
+   switch (action.type) {
+       case 'ADD_FRIEND':
+           return (
+               {
+                   friends: [
+                       ...state.friends, action.friend
+                    ]
+                }
+            )
+
+       case "REMOVE_FRIEND":
+           const IndexOfFriend = state.friends.findIndex(friend => friend.id === action.id);
+           return (
+               {
+                   ...state,
+                   friends: [
+                       ...state.friends.slice(0, IndexOfFriend),
+                       ...state.friends.slice(IndexOfFriend + 1)
+                   ]
+               }
+           )
+
+        default:
+            return state
+   }
 }
