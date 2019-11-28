dff4a51ecf5e8f23f6429b25bf0a2b68480d2ec7
Barbara Johnson <johnson.barbara@gmail.com>
2019-11-27 18:11:33 -0600

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,18 @@
+let state = {friends: []}
+
 export function manageFriends(state, action){
+
+    switch(action.type) {
+        case "ADD_FRIEND":
+            let friendsCopy = [...state.friends, action.friend]
+            return {friends: friendsCopy}
+        case "REMOVE_FRIEND":
+            let friendsReduced = state.friends.filter(friend => friend.id !== action.id)
+            return {friends: friendsReduced}
+        default:
+            return state;
+        }
+
 }
+
+ 
