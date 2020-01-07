bcb13768afd27fef9d4535e0e92cc98e002a6ff6
Felipe <fb.dc.col@gmail.com>
2020-01-07 20:27:26 -0300

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,31 +1,11 @@
-export function manageFriends(state = {
-    friends: [],
-  }, action) {
-    switch(action.type) {
-  
-      case "ADD_FRIEND":
-        return (
-          {...state,
-              friends: [
-                ...state.friends,
-                action.friend
-              ]
-          }
-      )
-  
-      case "REMOVE_FRIEND":
-        const removalIndex = state.friends.findIndex(friend => friend.id === action.id);
-        return (
-          {...state,
-              friends: [
-                ...state.friends.slice(0, removalIndex),
-                ...state.friends.slice(removalIndex + 1)
-              ]
-          }
-        )
-  
-      default:
-        return state;
-  
+export function manageFriends(state = {friends: []}, action){
+    switch(action.type){
+        case "ADD_FRIEND":
+            return {...state, friends:[...state.friends, action.friend]}
+        case "REMOVE_FRIEND":
+            let friendsArray = state.friends.filter((friend) => friend.id != action.id)
+            return {...state, friends: friendsArray}
+        default:
+            return state
     }
-  };
+}
