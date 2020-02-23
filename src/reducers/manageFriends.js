7e5fdaa503df68f5a901a03108c9e9ae83bf6460
Yingqi Chen <chanwk18@hotmail.com>
2020-02-23 01:08:21 +0000

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,10 +1,10 @@
-export function manageFriends(state, action){
-  switch(action.type){
-    case "ADD_FRIEND":
-      let a = state.friends
-      return a.push(action.friends)
-    case "REMOVE_FRIEND":
-      return {state.friends.filter(f=>f.id!=(action.id))}
-    default: return state
-  }
-}
+// export function manageFriends(state, action){
+//   switch(action.type){
+//     case "ADD_FRIEND":
+//       let a = state.friends
+//       return a.push(action.friends)
+//     case "REMOVE_FRIEND":
+//       return {state.friends.filter(f=>f.id!=(action.id))}
+//     default: return state
+//   }
+// }
--- test/index-test.js
+++ test/index-test.js
@@ -1,25 +1,25 @@
-// import { managePresents } from '../src/reducers/managePresents';
-// import { manageFriends } from '../src/reducers/manageFriends';
-// import sinon from 'sinon';
-//
-// describe('managePresents', () => {
-//
-//   let state = { numberOfPresents: 0 };
-//
-//   it("returns the existing state if the action's type doesn't match a type in the reducer", () => {
-//     expect(managePresents(state, { type: 'Random Action Type' })).toEqual(state);
-//   });
-//
-//   it("increases the number of presents if there the action's type is 'INCREASE'", () =>{
-//     expect(managePresents(state, { type: "INCREASE" })).toEqual({ numberOfPresents: 1 });
-//   });
-//
-//   it("adheres to the rules of being a pure function, by not changing the original state, and instead returning a new object", () =>{
-//     managePresents(state, { type: "INCREASE" });
-//     expect(state).toEqual({ numberOfPresents: 0 });
-//   })
-// })
-//
+import { managePresents } from '../src/reducers/managePresents';
+import { manageFriends } from '../src/reducers/manageFriends';
+import sinon from 'sinon';
+
+describe('managePresents', () => {
+
+  let state = { numberOfPresents: 0 };
+
+  it("returns the existing state if the action's type doesn't match a type in the reducer", () => {
+    expect(managePresents(state, { type: 'Random Action Type' })).toEqual(state);
+  });
+
+  it("increases the number of presents if there the action's type is 'INCREASE'", () =>{
+    expect(managePresents(state, { type: "INCREASE" })).toEqual({ numberOfPresents: 1 });
+  });
+
+  it("adheres to the rules of being a pure function, by not changing the original state, and instead returning a new object", () =>{
+    managePresents(state, { type: "INCREASE" });
+    expect(state).toEqual({ numberOfPresents: 0 });
+  })
+})
+
 // describe('manageFriends', () => {
 //   let state = {
 //     friends: [
@@ -59,26 +59,26 @@
 //     });
 //   });
 //   //
-//   // it("removes the friend when action type is 'REMOVE_FRIEND' and the action has a property of the friends id to be removed", () =>{
-//   //   let state = {
-//   //     friends: [
-//   //       {
-//   //         name: 'Avi',
-//   //         hometown: 'NYC',
-//   //         id: 100
-//   //       },
-//   //       {
-//   //         name: 'Joe',
-//   //         hometown: 'Boston',
-//   //         id: 101
-//   //       },
-//   //       {
-//   //         name: 'Steven',
-//   //         hometown: 'Philadephia',
-//   //         id: 102
-//   //       }
-//   //     ]
-//   //   };
+//   it("removes the friend when action type is 'REMOVE_FRIEND' and the action has a property of the friends id to be removed", () =>{
+//     let state = {
+//       friends: [
+//         {
+//           name: 'Avi',
+//           hometown: 'NYC',
+//           id: 100
+//         },
+//         {
+//           name: 'Joe',
+//           hometown: 'Boston',
+//           id: 101
+//         },
+//         {
+//           name: 'Steven',
+//           hometown: 'Philadephia',
+//           id: 102
+//         }
+//       ]
+//     };
 //
 //     expect(manageFriends(state, {
 //       type: "REMOVE_FRIEND",
