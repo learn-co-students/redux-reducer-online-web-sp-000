6bb6c33d85b4ad3566cb055ce7157ebf14342662
boom100100 <bsd231@nyu.edu>
2020-03-02 19:44:42 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,10 @@
 export function manageFriends(state, action){
+  switch(action.type){
+    case 'ADD_FRIEND':
+      return {...state, friends: [...state.friends, action.friend]}
+    case 'REMOVE_FRIEND':
+      return {...state, friends: state.friends.filter(e => e.id !== action.id)}
+    default:
+      return state
+  }
 }
