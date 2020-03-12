f648434a916456df68a52672b7cdb66cf9aa2ba6
ericabasak <ericabasak@gmail.com>
2020-03-12 14:07:48 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,39 @@
 export function manageFriends(state, action){
+  state = {
+    friends: []
+  }
+  switch(action.type) {
+    case "ADD_FRIEND":
+      return { friends: [ 
+        { 
+          name: 'Avi', 
+          hometown: 'NYC', 
+          id: 100 
+        }, 
+        { 
+          name: 'Joe', 
+          hometown: 'Boston', 
+          id: 101 
+        } ] 
+      }
+    case "REMOVE_FRIEND":
+      return { friends: [ 
+        { 
+          name: 'Avi', 
+          hometown: 'NYC', 
+          id: 100 
+        }, 
+        { 
+          name: 'Steven', 
+          hometown: 'Philadephia', 
+          id: 102 } ] 
+        }
+    default:
+      return { friends: [ 
+        { 
+          name: 'Avi', 
+          hometown: 'NYC', 
+          id: 100 } ] 
+        }
+  } 
 }
