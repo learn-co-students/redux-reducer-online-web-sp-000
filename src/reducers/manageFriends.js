13ad57e8d3c2205d31405900efbad73ce60999b1
soncrayon <arnold.curtis@gmail.com>
2020-03-17 20:50:10 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,12 @@
-export function manageFriends(state, action){
+export function manageFriends(state = {friends:[]}, action){
+    if (action.type === "ADD_FRIEND"){
+        let newFriends = state.friends 
+        newFriends.push(action.friend)
+        return state = {friends: newFriends}
+    } else if (action.type === "REMOVE_FRIEND") {
+        return state = {friends: state.friends.filter((friend) => {
+            return friend.id !== action.id
+        })}
+    }
+    return state 
 }
