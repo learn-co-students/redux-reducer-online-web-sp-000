99bd549ef7cfa3b119f124659e18e27d4e052146
Jane Venter <janeanneventer@gmail.com>
2020-03-22 22:38:04 -0700

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,15 @@
 export function manageFriends(state, action){
+    switch(action.type){
+        case 'ADD_FRIEND':
+            let newFriends = state.friends.slice();
+            newFriends.push(action.friend);
+            return { "friends": newFriends }
+        case 'REMOVE_FRIEND':
+            let i = state.friends.findIndex((friend)  => friend.id === action.id);
+            let friends = state.friends.slice();
+            friends.splice(i, 1);
+            return { "friends": friends }
+        default:
+            return state;
+    }
 }
