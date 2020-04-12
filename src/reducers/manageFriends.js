da19f0cd0ac35ff9980846b8a4f2c2d8b53f110f
Paul Rosero <paul_rosero@yahoo.com>
2020-04-12 01:03:15 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,25 @@
 export function manageFriends(state, action){
+    switch (action.type) {
+        case 'ADD_FRIEND' :
+        return Object.assign({}, state, {
+            friends: [
+                ...state.friends,
+                {
+                    name: action.friend.name,
+                    hometown: action.friend.hometown,
+                    id: action.friend.id
+                }
+            ]
+        })
+        case 'REMOVE_FRIEND' :
+        const index = state.friends.findIndex(friend => friend.id === 101)
+        return Object.assign({}, state, {
+            friends: [
+                ...state.friends.slice(0, index),
+                ...state.friends.slice(index + 1)
+            ]
+        })
+        default:
+            return state
+    }
 }
