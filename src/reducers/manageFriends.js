e7ad7ca4b6d33d530d1743e8e86b7d85692738c2
jeremyamram <52257264+jeremyamram@users.noreply.github.com>
2020-04-18 12:25:47 +0100

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,28 @@
-export function manageFriends(state, action){
+```js
+  action = {
+    type: "ADD_FRIEND",
+    friend: {
+      name: "Chrome Boi"
+      homewtown: "NYC",
+      id: 1
+    }
+  }
+```
+
+```js
+  action = {
+    type: "REMOVE_FRIEND",
+    id: 1
+  }
+```
+
+export function manageFriends(state = {friends: []}, action){
+    switch (action.type) {
+        case 'ADD_FRIEND':
+          return { friends: [...state, action.friend] };
+        case 'REMOVE_FRIEND':
+            return { friends: state.friends.filter((friend) => friend.id != action.id ) };
+        default:
+          return state;
+      }
 }
