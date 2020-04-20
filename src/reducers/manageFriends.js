e991350cad30719d5eeffcfc709c614cfc15095c
oliverpdahl <oliverpdahl@gmail.com>
2020-04-20 13:44:35 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,16 @@
 export function manageFriends(state, action){
+  switch (action.type) {
+    case 'ADD_FRIEND':
+      return { friends: [...state.friends, {name: action.friend.name, hometown: action.friend.hometown, id: action.friend.id}]}
+    case 'REMOVE_FRIEND':
+      let newFriends = [...state.friends]
+      const deleteFriend = newFriends.find(e => e.id === action.id)
+      const index = newFriends.indexOf(deleteFriend)
+      if(index > -1){
+        newFriends.splice(index, 1)
+      }
+      return {friends: newFriends}
+    default:
+      return { friends: [ { name: 'Avi', hometown: 'NYC', id: 100 }]}
+  }
 }
