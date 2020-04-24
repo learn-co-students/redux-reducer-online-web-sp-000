9de19f87f47e758d2f4e78609548957e72e4a9cb
Blythe Pollard <blytheepollard@gmail.com>
2020-04-24 17:43:45 -0600

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,19 @@
-export function manageFriends(state, action){
+export function manageFriends(state = {friends: []}, action){
+    switch (action.type) {
+        case 'ADD_FRIEND':
+            return {friends: {...state.friends}}
+    }
 }
+
+
+
+
+// export function managePresents(state = {numberOfPresents: 0}, action) {
+//     switch (action.type) {
+//         case 'INCREASE':
+//             return {numberOfPresents: state.numberOfPresents + 1} 
+//         default: 
+//             return state;        
+//     }
+// }
+
