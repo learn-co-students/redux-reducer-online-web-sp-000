69f32b6fa324a91663734ace771e7b4716186cf7
Blythe Pollard <blytheepollard@gmail.com>
2020-04-24 17:46:57 -0600

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,7 +1,7 @@
 export function manageFriends(state = {friends: []}, action){
     switch (action.type) {
         case 'ADD_FRIEND':
-            return {friends: {...state.friend}}
+            return {friends: {...state.friend, hometown: state.friend.hometown}}
     }
 }
 //wanting to overrwrite state.friends data- but with what?
