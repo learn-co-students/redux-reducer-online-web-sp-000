6a2cd5cedcb4abeb77ab358f36ab6c20552627e5
Blythe Pollard <blytheepollard@gmail.com>
2020-04-24 18:31:48 -0600

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,9 +1,19 @@
 export function manageFriends(state = {friends: []}, action){
     switch (action.type) {
         case 'ADD_FRIEND':
-            return {friends: {...state.friend, hometown: state.friend.hometown}}
+            return ({...state, friends: [...state.friends, action.friend ] })
+        case 'REMOVE_FRIEND':
+            const badFriendIndex = state.friends.findIndex(friend => friend.id === action.id)
+            return ( 
+                {...state, 
+                    friends: [
+                    ...state.friends.slice(0, badFriendIndex), //this returns what's before badfriend
+                    ...state.friends.slice(badFriendIndex + 1) //this returns what's after badfriend--together, returns rest of array, i think?
+                ]}
+                )    
+        default:
+            return state; 
     }
 }
-//wanting to overrwrite state.friends data- but with what?
 
 
