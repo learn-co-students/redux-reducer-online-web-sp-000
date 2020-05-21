bf13cc68843b852bdfd38765ac5acd07155a2112
github username <github email address>
2020-05-21 01:20:01 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,17 @@
 export function manageFriends(state, action){
+  let newFriendsArray = state.friends.slice();
+  switch (action.type) {
+    case 'ADD_FRIEND':
+      newFriendsArray.push(action.friend);
+      return {friends: (newFriendsArray)};
+    case 'REMOVE_FRIEND':
+      let friendToBeRemoved = newFriendsArray.find(friend => friend.id === action.id);
+      newFriendsArray.splice(newFriendsArray.indexOf(friendToBeRemoved), 1);
+      return {friends: newFriendsArray};
+
+    default:
+      return state;
+  }
 }
+
+let state = { friends: [] };
--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,16 @@
 export function managePresents(state, action){
+  // if (action.type === 'INCREASE') {
+  //   return {numberOfPresents: state.numberOfPresents + 1}
+  // } else {
+  //   return state;
+  // }
+  switch (action.type) {
+    case 'INCREASE':
+      return {numberOfPresents: state.numberOfPresents + 1};
+    default:
+      return state;
+  }
+
 }
+
+let state = { numberOfPresents: 0 };
