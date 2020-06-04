a983498ee10dd2bb7f2a28e6ab3d290aa2a8fbf3
Ben Tagtow <bentagtow@gmail.com>
2020-06-04 13:20:52 -0600

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,24 @@
-export function manageFriends(state, action){
+export function manageFriends(state = {friends:[]}, action){
+
+    switch (action.type){
+        case 'ADD_FRIEND' : 
+            return { 
+                friends: 
+                    [...state.friends, 
+
+                        ({
+                            name: action.friend.name,
+                            hometown: action.friend.hometown,
+                            id: action.friend.id                           
+                        })                      
+                   ]
+                }
+        case 'REMOVE_FRIEND' : 
+            return {
+                friends: 
+                    [...state.friends.filter(friend => action.id !== friend.id)]
+            }
+        default: return state
+    
+    }
 }
