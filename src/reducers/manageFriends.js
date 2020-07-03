00a88a45262d2f68c019514376ccf83e657bbcd7
azbury <acasbury25@aol.com>
2020-07-03 09:27:41 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,18 @@
 export function manageFriends(state, action){
+    switch (action.type) {
+        case 'REMOVE_FRIEND':
+            for (let i = 0; i < state.friends.length; i++) {
+                if (state.friends[i].id === action.id) {
+                    return (
+                        {...state, friends: [...state.friends.slice(0, i), ...state.friends.slice(i + 1)]}
+                    )
+                }
+            }
+        case 'ADD_FRIEND':
+            return (
+                {...state, friends: [...state.friends, action.friend]}
+            )
+        default:
+            return state
+    }
 }
