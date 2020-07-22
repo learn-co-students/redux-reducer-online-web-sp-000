6fdf707a4d90eb06d82853650c2281b9c65f4f44
nathan-stamand <nathansaint810@gmail.com>
2020-07-22 15:16:15 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,13 @@
-export function manageFriends(state, action){
+export function manageFriends(state = {friends: []}, action){
+  const newArray = state.friends.slice()
+  switch (action.type) {
+    case 'ADD_FRIEND':
+      newArray.push(action.friend)
+      return {friends: newArray}
+    case 'REMOVE_FRIEND':
+      let newerArray = newArray.filter(friend => friend.id != action.id)
+      return {friends: newerArray}
+    default:
+      return state;
+  }
 }
