8d89d676bfbe5d1ba45ff2739b0b5704b12cbb14
Josh Ellis <joshe523@gmail.com>
2020-08-03 09:02:40 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,16 @@
-export function manageFriends(state, action){
+export function manageFriends (state = { friends: [] }, action) {
+  switch (action.type) {
+    case 'ADD_FRIEND':
+      return { ...state, friends: [...state.friends, action.friend] }
+    case 'REMOVE_FRIEND':
+      const arr = []
+      state.friends.forEach(f => {
+        if (f.id !== action.id) {
+          arr.push(f)
+        }
+      })
+      return { ...state, friends: arr }
+    default:
+      return state
+  }
 }
