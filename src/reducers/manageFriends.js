b4a366290a62b1a6a40989f0cb8adfc2ba69af87
nicholas-wilson <nicholas.wilson@nevada.unr.edu>
2020-08-10 14:47:19 -0700

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,16 @@
-export function manageFriends(state, action){
+export function manageFriends(state={friends: []}, action){
+  switch(action.type) {
+    case "ADD_FRIEND":
+      return {friends: [...state.friends, action.friend]};
+    case "REMOVE_FRIEND":
+      let lessFriends = [...state.friends];
+      for (let i = 0; i < state.friends.length; i++) {
+        if (state.friends[i].id === action.id) {
+          lessFriends.splice(i, 1)
+        }
+      }
+      return {friends: lessFriends};
+    default:
+      return state;
+  }
 }
