8056d8c46915b61380321a39522692b805779f60
github username <github email address>
2020-09-27 16:12:53 -0500

    Done.

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,25 @@
+
+
+
+
+
+
 export function managePresents(state, action){
-}
+
+    // action = {
+    //     type: "INCREASE"
+    // }
+    
+    // state = {
+    //     numberOfPresents: 0
+    // }
+
+     switch (action.type) {
+        case 'INCREASE':
+          return {numberOfPresents: state.numberOfPresents + 1}
+        default:
+          return state;
+      }
+    }
+
+
