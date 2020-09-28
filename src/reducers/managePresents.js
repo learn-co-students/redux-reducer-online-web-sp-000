96bb4ad861d3e641f47845e77d3d7f4daac68625
Alvin Coulson <aaacoulson@gmail.com>
2020-09-28 16:37:23 -0400

    Done.

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,9 @@
 export function managePresents(state, action){
+    if(action.type === "INCREASE"){
+        return {numberOfPresents: state.numberOfPresents + 1}
+    }
+    return state;
 }
+
+let state = { numberOfPresents: 0 };
+let action = { type: "INCREASE"}
