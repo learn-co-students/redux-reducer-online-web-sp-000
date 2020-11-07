b91743320c56ebeb55de56153eb3b1df16ff8e00
ting682 <tchung682@gmail.com>
2020-11-06 19:02:51 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,18 @@
 export function manageFriends(state, action){
+    switch(action.type) {
+        case 'ADD_FRIEND':
+            return {friends: [...state.friends, action.friend]}
+        case 'REMOVE_FRIEND': {
+            console.log(action.id);
+            return {friends: state.friends.filter(function (friend) {
+                
+                return friend.id !== action.id
+            })}
+            //console.log(friendIndex);
+            //return {friends: state.friends.slice(friendIndex)}
+        }
+            
+        default: 
+            return state
+    }
 }
