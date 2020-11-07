6529a8e1266e21b3cfc6e1e970faa747548acb0b
Christopher Dent <christopherdent01@gmail.com>
2020-11-06 22:14:45 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,29 @@
+let state = {friends: []}
+let action = {
+    type: "ADD_FRIEND",
+    friend: {
+      name: "Chrome Boi",
+      homewtown: "NYC",
+      id: 1
+    }
+  }
+
 export function manageFriends(state, action){
+
+    switch (action.type) {
+      case 'ADD_FRIEND':
+         return {friends: [...state.friends, {...action.friend}]}
+
+        break;
+        case 'REMOVE_FRIEND':
+          return {friends: state.friends.filter(friend => friend.id !== action.id)}
+          break;
+      default:
+        return state;
+    }
 }
+
+
+// function changeState(state, action){
+
+// }
--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,16 @@
+
+let state = {numberOfPresents: 0}
+let action = {
+     type: 'INCREASE'
+   }
+
+
 export function managePresents(state, action){
+  switch (action.type) {
+    case 'INCREASE':
+      return {numberOfPresents: state.numberOfPresents + 1}
+      break;
+      default:
+        return state;
+  }
 }
