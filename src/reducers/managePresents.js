17ed2675d3f7200b701a22a444f6cb45a9a78623
Pancho Labrador <plabradorjr@gmail.com>
2020-11-19 22:39:33 -0600

    Done.

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,14 +1,14 @@
 export function managePresents(state = {
-    numberOfPresents: 0,
-  }, action) {
-    switch(action.type) {
-  
-      case "INCREASE":
-        return Object.assign({}, state, {
-          numberOfPresents: state.numberOfPresents + 1
-        });
-  
-      default:
-        return state;
-    }
-  };
+  numberOfPresents: 0,
+}, action) {
+  switch(action.type) {
+
+    case "INCREASE":
+      return Object.assign({...state,
+        numberOfPresents: state.numberOfPresents + 1
+      });
+
+    default:
+      return state;
+  }
+};
