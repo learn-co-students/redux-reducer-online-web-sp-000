69092fe83268b9ec495b1e3f5295dcdb220e6649
jessicagomesng <jgnpeluso@gmail.com>
2020-11-28 16:43:30 +0000

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,15 @@
-export function manageFriends(state, action){
+export function manageFriends(state = {friends: []}, action){
+    switch (action.type) {
+        case 'ADD_FRIEND':
+            return {
+                friends: state.friends.concat(action.friend)
+            };
+        case 'REMOVE_FRIEND':
+            let newList = state.friends.filter(friend => friend.id !== action.id);
+            return {
+                friends: newList
+            };
+        default:
+            return state;
+    }
 }
