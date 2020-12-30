df99379ba3b08e9efcddb8c18fd4fbd4fe5ab5ac
ksummerill <kaitlynsummerill@gmail.com>
2020-12-30 13:24:35 -0800

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,24 @@
+let state = {friends: []}
+
 export function manageFriends(state, action){
+
+  switch (action.type) {
+    case 'ADD_FRIEND':
+      const addFriend = {
+        name: action.friend.name,
+        hometown: action.friend.hometown,
+        id: action.friend.id
+      }
+      const updateState = {...state}
+      updateState.friends.push(addFriend)
+      return updateState
+    case 'REMOVE_FRIEND':
+      const id = action.id
+        return {
+          ...state,
+          friends: state.friends.filter(friend => friend.id !== id)
+        }
+    default:
+      return state;
+  }
 }
--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,13 @@
+// Set an default value for the state argument - an object with a key, numberOfPresents, assigned to 0
+// If the reducer receives a type set to "INCREASE", return a new state where the value of numberOfPresents is increased by one.
+
 export function managePresents(state, action){
+  state = {numberOfPresents: 0}
+
+  switch (action.type) {
+    case 'INCREASE':
+      return {numberOfPresents: state.numberOfPresents + 1}
+    default:
+      return state;
+  }
 }
