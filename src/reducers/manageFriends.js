af2cbe036a68a999165137f7eb1d2acd2616daef
Dwayne Weller <mr_dwayne_w@yahoo.com>
2021-01-06 07:12:44 +0000

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,14 @@
 export function manageFriends(state, action){
+  switch (action.type) {
+    case 'ADD_FRIEND':
+      return {
+        ...state,
+        {
+          name: action.friend.name,
+          hometown: action.friend.hometown,
+          id: action.friend.id
+        }
+      }
+
+  }
 }
