bf3a211060cff3f1e6e876f2f439b6dc2e46a270
Snowing222 <snowing222@gmail.com>
2021-01-18 10:21:34 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,16 @@
 export function manageFriends(state, action){
+    let newState = Object.assign({}, state)
+
+    switch(action.type){
+        case "ADD_FRIEND":
+            newState.friends.push(action.friend)
+            return newState
+        case "REMOVE_FRIEND":
+          
+            newState.friends = newState.friends.filter(friend=>friend.id!==action.id)
+            console.log(newState)
+            return newState
+        default:
+            return state
+    }
 }
