df40bb4a6cc33132905b36c9032d12cd8214739e
jenleebeaver <jen.lee.beaver@gmail.com>
2021-03-05 14:16:31 +0530

    Done.

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,12 @@
+
+let state = {numberOfPresents: 0}
+let action = {type: 'INCREASE'}
+
 export function managePresents(state, action){
+    switch (action.type){
+        case 'INCREASE':
+            return {count: state.count + 1} 
+        default: 
+            return state; 
+    }
 }
