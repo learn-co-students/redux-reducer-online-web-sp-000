06379ea56921f229d8d16264fac423f62fdb5cc1
CoderJay06 <jaycru06@yahoo.com>
2021-03-12 05:42:53 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,3 +1,29 @@
 export function manageFriends(state, action){
-
+   // returns the existing state if the action's type doesn't match a type in the reducer:
+   // adds the friend when type is 'ADD_FRIEND' and the action has a friend property with a name, hometown and id:
+   // removes the friend when action type is 'REMOVE_FRIEND' and the action has a property of the friends id to be removed:
+   switch(action.type) {
+      case 'ADD_FRIEND':
+         // When action has a friend property, add them with state's friends list to new object, return it
+         return {
+            friends: [
+               ...state.friends, 
+               {
+                  name: action.friend.name,
+                  hometown: action.friend.hometown,
+                  id: action.friend.id
+               }
+            ]
+         };
+      case 'REMOVE_FRIEND':
+         // When action type remove and action has friend property, return new object without that friend
+         // Traverse friends array until friend to remove is found. Remove friend and return new friends obj
+         // return {
+         //    friends: state.friends.map((friend) => {
+         //       return friend.id !== action.friend.id;
+         //    })
+         // };
+      default:
+         return state;
+   }
 }
--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,9 +1,10 @@
 export function managePresents(state = { numberOfPresents: 0 }, action){
    switch (action.type) {
       case 'INCREASE':
-         const newState = Object.assign({}, state);
-         newState.numberOfPresents++;
-         return newState
+         // const newState = Object.assign({}, state);
+         // newState.numberOfPresents++;
+         // return newState
+         return { numberOfPresents: state.numberOfPresents + 1 };
       default:
          return state;
    }
