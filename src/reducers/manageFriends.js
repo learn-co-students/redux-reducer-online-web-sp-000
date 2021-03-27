ffbcd5e75f91ed99cb42b87971a39b0a73e2e616
john million <jpmillion@comcast.net>
2021-03-27 16:39:53 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,15 @@
-export function manageFriends(state, action){
+export function manageFriends(state = {friends: []}, action){
+    switch(action.type) {
+        case 'ADD_FRIEND':
+            return {
+                friends: [...state.friends, {name: action.friend.name, hometown: action.friend.hometown, id: action.friend.id}]
+            }
+        case 'REMOVE_FRIEND':
+            const i = state.friends.findIndex(e => e.id === action.id)
+            return {
+                friends: [...state.friends.slice(0, i), ...state.friends.slice(i+1)]
+            }
+        default:
+            return state
+    }
 }
