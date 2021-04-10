73a72b85a46dca432f9f2ab6fa449365203d525a
khoinguyenkc <khoinguyenkc@gmail.com>
2021-04-10 05:30:16 -0700

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,17 @@
 export function manageFriends(state, action){
+  switch (action.type) {
+    case 'ADD_FRIEND':
+
+    // state = { friends: [ {firend1}, {firend2}]}
+    return { friends: [ ...state.friends, action.friend] }
+    break;
+    case 'REMOVE_FRIEND':
+    //CODE
+    return { friends: state.friends.filter( object => {
+      return object.id !== action.id
+    })}
+    break;
+    default:
+    return state;
+  }
 }
