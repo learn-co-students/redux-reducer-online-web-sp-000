2dc8e7cb32e479fe9e88f04fd4459af4f755b2df
Neil E <argus.two.2@protonmail.com>
2021-05-11 15:30:15 -0700

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,12 @@
-export function manageFriends(state, action){
+export function manageFriends(state={friends: []}, action){
+  if (action.type === "ADD_FRIEND") {
+    {friends: state.friends.push(action.friend)}
+  } else if (action.type === "REMOVE_FRIEND") {
+    const index = state.friends.findIndex(x => x.id === action.id);
+    return ({
+      ...state,
+      friends: [...state.friends.slice(0, index), ...state.friends.slice(index + 1)]
+    })
+  }
+  return state;
 }
