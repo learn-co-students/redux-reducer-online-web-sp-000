ff96a3b68c3c470cc5c5e39bad7be97dbd4beadf
nlanese21 <nlanese21@gmail.com>
2021-05-27 15:28:20 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,14 @@
 export function manageFriends(state, action){
+    switch(action.type){
+        case "ADD_FRIEND":
+            return {friends: [...state.friends, action.friend]}
+        case "REMOVE_FRIEND":
+            return {friends: state.friends.map(person => {
+                if (person.id != action.id){
+                    return person
+                }
+            })}
+        default:
+            return state
+    }
 }
