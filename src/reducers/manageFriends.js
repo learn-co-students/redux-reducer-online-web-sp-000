4d1ce4ec99a54069c1e89207715ff5aed0272c00
TrueC-D <aletheacary@gmail.com>
2021-06-17 00:44:52 -0700

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,30 @@
-export function manageFriends(state, action){
+export function manageFriends(state = {friends: []}, action){
+    switch(action.type){
+        case 'ADD_FRIEND':
+            return (
+                {...state, friends: [
+                    ...state.friends, action.friend
+                ]}
+            )
+            // return {friends: state.assign({}, friends, {friend: {
+            //     name: action.friend.name,
+            //     hometown: action.friend.hometown,
+            //     id: action.friend.id
+            // }} )};
+        case 'REMOVE_FRIEND':
+            const removeIdx = state.friends.findIndex(friend => friend.id ===action.id);
+            return(
+                {...state, friends:[
+                    ...state.friends.slice(0, removeIdx),
+                    ...state.friends.slice(removeIdx +1)
+                ]}
+            )
+            // return {friends: state.map(friend => {
+            //     if(friend.id != action.id){return friend}
+            // })}
+        default:
+            return state
+    
+
+    }
 }
