22e17fa24bcedbfc5de891b5e959897a194c303c
yarvinh <yarvinhhernandez@gmail.com>
2021-06-18 22:46:52 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,23 @@
 export function manageFriends(state, action){
+    switch (action.type) {
+        case "ADD_FRIEND":
+           return {     
+               friends: [...state.friends,action.friend]
+            }
+        case  "REMOVE_FRIEND":
+           
+            const copy = state.friends.filter((friend)=> {
+                if (friend.id !== action.id){
+                    return friend
+                }
+            })
+           return {
+            friends: copy
+           }
+           default:
+           return state
+    }
+
 }
+
+
