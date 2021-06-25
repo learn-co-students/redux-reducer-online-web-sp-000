3ed5c85b06c0a20d0722b31d0db7443cd872c33e
AlexJMartinez <alexandr3martin3z@gmail.com>
2021-06-24 17:28:16 -0700

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,28 @@
 export function manageFriends(state, action){
+    switch (action.type) {
+        case 'ADD_FRIEND':
+          return {friends: state.friends + 1}
+        case 'REMOVE_FRIEND':
+          return {friends: state.friends - 1}
+        default:
+          return state;
+    } 
+}
+
+let state = {
+    friends: []
+}
+
+let increase = {
+    type: "ADD_FRIEND",
+    friend: {
+        name: "Chrome Boi", 
+        hometown: "NYC",
+        id: 1
+    }
+}
+
+let decrease = {
+    type: "REMOVE_FRIEND", 
+    id: 1
 }
